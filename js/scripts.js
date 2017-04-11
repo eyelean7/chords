3de035375f4d98ef9d45c6aa37707116c45e21b1
Anna Horodetska and Ilene Gorski <anna+ilene@>
2017-04-11 10:05:48 -0700

    add minor progression

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ var minor = ["i", "iidim", "III", "iv", "V", "VI", "bVII"];
 var notes = ["A", "B", "C", "D", "E", "F", "G"];
 
 function buildTonality(){
+  var mode = "minor";
   var key = "F";
   // change key to user input
   var end = notes.slice(0,notes.indexOf(key));
@@ -12,12 +13,23 @@ function buildTonality(){
     begin.push(end[i]);
   }
   function buildMode(){
-    for (i=0; i<begin.length; i++){
-      if (i=== 1 || i=== 2 || i=== 5 || i=== 6 ){
-        begin[i] = begin[i].toLowerCase();
-        console.log(begin);
+    if (mode ==="major"){
+      for (i=0; i<begin.length; i++){
+        if (i=== 1 || i=== 2 || i=== 5 || i=== 6 ){
+          begin[i] = begin[i].toLowerCase();
+          console.log(begin);
+        }
       }
     }
+    else {
+      for (i=0; i<begin.length; i++){
+        if (i=== 0 || i=== 1 || i=== 3){
+          begin[i] = begin[i].toLowerCase();
+          console.log(begin);
+        }
+      }
+    }
+
   }
   buildMode();
   return begin;
